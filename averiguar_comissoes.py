5d91446088b4a01748f8b4f157b71ef409ca81e5
Gustavo Massamichi Nakamura <gmassamichi@gmail.com>
2025-08-05 17:23:46 -0300

    nova arquivo

--- averiguar_comissoes.py
+++ averiguar_comissoes.py
@@ -314,7 +314,7 @@ def _ajustar_para_devolucao(valor, is_devolucao):
     return valor if not is_devolucao else -valor
 
 def processar_planilhas():
-    caminho_origem = r"C:\Users\win11\OneDrive\Documentos\Margens de fechamento\Margem_250731 - wapp.xlsx"
+    caminho_origem = r"C:\Users\win11\OneDrive\Documentos\Margens de fechamento\Margem_250731 - Fechamento - wapp.xlsx"
     caminho_downloads = os.path.join(os.path.expanduser('~'), 'Downloads', 'Averiguar_Comissoes (MARGEM).xlsx')
     
     try:
