ef7a13bad9b79ea1b46779fc1f4b51496a050652
Gustavo Massamichi Nakamura <gmassamichi@gmail.com>
2025-11-04 08:50:49 -0300

    Atualização de regra do Rossi

--- averiguar_comissoes.py
+++ averiguar_comissoes.py
@@ -312,7 +312,8 @@ def aplicar_regras_comissao_fixa(row, regras):
             return _ajustar_para_devolucao(0, is_devolucao)
         
         # TERCEIRO verifica a regra de 2%
-        if grupo_produto in ['MIUDOS BOVINOS', 'SUINOS', 'SALGADOS SUINOS A GRANEL']:
+        if grupo_produto in ["MIUDOS BOVINOS", "SUINOS", "SALGADOS SUINOS A GRANEL", 
+                             "SALGADOS SUINOS EMBALADOS", "CORTES DE FRANGO"]:
             return _ajustar_para_devolucao(2, is_devolucao)
         
         if codproduto == 700:
@@ -374,7 +375,7 @@ def _ajustar_para_devolucao(valor, is_devolucao):
     return valor if not is_devolucao else -valor
 
 def processar_planilhas():
-    caminho_origem = r"C:\Users\win11\Downloads\Margem_251030 - wapp.xlsx"
+    caminho_origem = r"C:\Users\win11\Downloads\Margem_251031 - wapp.xlsx"
     caminho_downloads = os.path.join(os.path.expanduser('~'), 'Downloads', 'Averiguar_Comissoes (MARGEM).xlsx')
     
     try:
