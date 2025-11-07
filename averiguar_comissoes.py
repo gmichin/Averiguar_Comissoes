425f03d072ca4806a884fcd7594de593cce6b7cc
Gustavo Massamichi Nakamura <gmassamichi@gmail.com>
2025-11-07 13:59:07 -0300

    nova regra de comissão

--- averiguar_comissoes.py
+++ averiguar_comissoes.py
@@ -184,6 +184,11 @@ def criar_regras_comissao_fixa():
                     'grupos_produto': ['SALAME UAI']
                 },
             },
+            'REDE AYUMI': {
+                0.03: {
+                    'grupos_produto': ['SALAME UAI']
+                },
+            },
             'REDE ROLDAO': {
                 0.02: {
                     'grupos_produto': [
@@ -375,7 +380,7 @@ def _ajustar_para_devolucao(valor, is_devolucao):
     return valor if not is_devolucao else -valor
 
 def processar_planilhas():
-    caminho_origem = r"C:\Users\win11\Downloads\Margem_251031 - wapp.xlsx"
+    caminho_origem = r"C:\Users\win11\Downloads\MRG_251106 - wapp.xlsx"
     caminho_downloads = os.path.join(os.path.expanduser('~'), 'Downloads', 'Averiguar_Comissoes (MARGEM).xlsx')
     
     try:
